e6910231a274590a21ac0b19da60d655d5758495
claclarissa12 <clarissadewi490@gmail.com>
2026-01-13 15:28:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const clarissa = {
+    nama_lengkap: "clarissa dewi sn",
+    kelas: "XI RPL 3",
+    sekolah: "SMK TARUNA BHAKTI",
+    menikah: false,
+    alamat: "undefined",
+
+    lahir: {
+        kota: "depok",
+        tanggal: "10-12-2008",
+        umur: 17
+    },
+
+    kesukaan: ["nonton", "main", "seblak"],
+
+    
+};
+clarissa.alamat= "jl taruna bhakti"; 
+clarissa.hobi="bulutangkis"; 
+console.log(clarissa);
